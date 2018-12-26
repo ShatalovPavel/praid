2a862ba5edb3c856942be11fbe0a532578b40e67
ShatalovPavel <shatalov.tester@gmail.com>
2018-12-26 11:59:11 +0200

    cut code from  construct in method execute

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,63 +12,7 @@ class IndexController extends Controller
 {
     //
     public function __construct(){
-    	$nameCurrency = 'USD';
-    	$day = 14;
-
-    	$startDate = date("Ymd", mktime(0, 0, 0, date('m'), date('d') - $day, date('Y')));
-    	$endDate = date("Ymd"); 
-    	$currency = Currency::where('name',mb_strtolower($nameCurrency))->get();
-
-    	$currencyID = $currency[0]->id;
-
-    	$priceCurrentID = Price::where('currency_id',$currencyID)->first();
-    		
-    		if(empty($priceCurrentID)){
-    			$data = $this->getDataCurrency($nameCurrency,$startDate,$endDate,$day);
-    			$dates = $this->getDataDate($day);
-    			for ($i=0; $i <count($data); $i++) { 
-    				$input = [
-    					'price' => $data[$i],
-    					'currency_id' => $currencyID,
-    					'Data' => $dates[$i]
-    				];
-    				$price = new Price();
-    				$price->fill($input);
-    				$price->save();
-    			}
-
-    		}
-    		else{
-
-    			$day=1;
-    			$flag=FALSE;
-    		    $currentDay = $this->getDataDate($day);
-    		    $dates = Price::pluck('Data');
-    			$data = $this->getDataCurrency($nameCurrency,FALSE,$endDate,FALSE);
-
-    			foreach ($dates as $date) {
-    				if($date == $currentDay){
-    					$flag=FALSE;
-    					break;
-    				}
-    				else{
-    					$flag=TRUE;
-    				}
-    			}
-    			if($flag){
-    				$input = [
-    						'price' => $data[0]->rate,
-    						'currency_id' => $currencyID,
-    						'Data' => $currentDay 
-    				];
-    				
-    				$price = new Price();
-    				$price->fill($input);
-    				$price->save();
-
-    			}
-
-    		}
+    	
     
     }
     public function chart(){
@@ -82,6 +26,64 @@ class IndexController extends Controller
     		
     }
     public function execute(){
+        
+        $nameCurrency = 'USD';
+        $day = 14;
+
+        $startDate = date("Ymd", mktime(0, 0, 0, date('m'), date('d') - $day, date('Y')));
+        $endDate = date("Ymd"); 
+        $currency = Currency::where('name',mb_strtolower($nameCurrency))->get();
+
+        $currencyID = $currency[0]->id;
+
+        $priceCurrentID = Price::where('currency_id',$currencyID)->first();
+            
+            if(empty($priceCurrentID)){
+                $data = $this->getDataCurrency($nameCurrency,$startDate,$endDate,$day);
+                $dates = $this->getDataDate($day);
+                for ($i=0; $i <count($data); $i++) { 
+                    $input = [
+                        'price' => $data[$i],
+                        'currency_id' => $currencyID,
+                        'Data' => $dates[$i]
+                    ];
+                    $price = new Price();
+                    $price->fill($input);
+                    $price->save();
+                }
+
+            }
+            else{
+
+                $day=1;
+                $flag=FALSE;
+                $currentDay = $this->getDataDate($day);
+                $dates = Price::pluck('Data');
+                $data = $this->getDataCurrency($nameCurrency,FALSE,$endDate,FALSE);
+
+                foreach ($dates as $date) {
+                    if($date == $currentDay){
+                        $flag=FALSE;
+                        break;
+                    }
+                    else{
+                        $flag=TRUE;
+                    }
+                }
+                if($flag){
+                    $input = [
+                            'price' => $data[0]->rate,
+                            'currency_id' => $currencyID,
+                            'Data' => $currentDay 
+                    ];
+                    
+                    $price = new Price();
+                    $price->fill($input);
+                    $price->save();
+
+                }
+
+            }
 
     	$nameCurrency = 'USD';
     	$currencies = Currency::all();
